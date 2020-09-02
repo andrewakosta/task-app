4e150e7fc755b6c0dcb2a4a09d100e3694b9eea8
Andres Acosta <andresakosta@outlook.com>
2020-09-02 12:01:38 -0500

    Login rendered

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
 const Login = () => {
+    const onChange = e =>{
+        console.log('redux')
+    }
     return ( 
-        <h1>From Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Log in</h1>
+                <form >
+                    <div className="campo-form">
+                        <label htmlFor="email">E-mail</label>
+                        <input 
+                            type="email"
+                            id='email'
+                            placeholder='Insert your email'
+                            onChange={onChange}
+                            autoComplete='off'
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id='password'
+                            placeholder='Insert yout password'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input type="submit" className='btn btn-primario btn-block'
+                               value='Log in' 
+                        />
+
+                    </div>
+                </form>
+            </div>
+        </div>
      );
 }
  
