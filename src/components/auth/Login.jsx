900d698651c0bbe76d3134690080a38e865cb4f9
Andres Acosta <andresakosta@outlook.com>
2020-09-02 12:19:09 -0500

    State login added

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
+
 const Login = () => {
+
+    const [user, setUser] = useState({
+        email:'',
+        password:''
+    })
+
+    const {email, password} = user
+
     const onChange = e =>{
-        console.log('redux')
+        setUser({
+            ...user,
+            [e.target.name]:e.target.value
+        })
+    }
+    const handleSubmit = e => {
+        e.preventDeafault()
+        console.log(email, password)
     }
     return ( 
         <div className="form-usuario">
             <div className="contenedor-form sombra-dark">
                 <h1>Log in</h1>
-                <form >
+                <form  onSubmit={handleSubmit}>
                     <div className="campo-form">
                         <label htmlFor="email">E-mail</label>
                         <input 
                             type="email"
                             id='email'
                             placeholder='Insert your email'
+                            name='email'
+                            value={email}
                             onChange={onChange}
                             autoComplete='off'
                         />
@@ -24,6 +43,8 @@ const Login = () => {
                             type="password"
                             id='password'
                             placeholder='Insert yout password'
+                            name='password'
+                            value={password}
                             onChange={onChange}
                         />
                     </div>
@@ -31,9 +52,11 @@ const Login = () => {
                         <input type="submit" className='btn btn-primario btn-block'
                                value='Log in' 
                         />
-
                     </div>
                 </form>
+                <Link to={'/register'} className='enlace-cuenta'>
+                    Get account
+                </Link>
             </div>
         </div>
      );
