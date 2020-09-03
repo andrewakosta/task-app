975996f6e3b315e95e2e5a3fb2302e8cdf455193
Andres Acosta <andresakosta@outlook.com>
2020-09-03 18:30:50 -0500

    get from context the current project

--- src/components/tasks/ListTasks.jsx
+++ src/components/tasks/ListTasks.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
+import ContextProject from '../../context/projects/ContextProject'
 import Task from './Task'
 const ListTasks = () => {
+
+    const {currentProject} =  useContext(ContextProject)
+    if(!currentProject){
+        return <h2>Please choose one project</h2>
+    } 
+    const [project] = currentProject
+    //List tasks
     const tasks = [
         {name:'Choose the server', status:false},
         {name:'add every coder to buil porject', status:true},
         {name:'make the desing to porejct', status:false}
     ]
+   
     return ( 
         <>
-            <h2>Project: Virtual Store</h2>
+            <h2>{project.name}</h2>
             <ul className='listado-tareas'>
                 {
                     tasks.length === 0 
